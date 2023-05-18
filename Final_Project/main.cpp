8cffc8db41efc4e988d88fc0e6e4e35a4df0a1cf
詹景森 <91170862+Leon9453@users.noreply.github.com>
2023-05-18 17:07:23 +0800

    Add files via upload

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -1,65 +1,97 @@
-
 #include <stdio.h>
 #include <GL/glut.h>
 #include "glm.h"
-
 GLMmodel * head = NULL;
 GLMmodel * body = NULL;
-GLMmodel * uparmR = NULL;
-GLMmodel * lowarmR = NULL;
-float teapotX = 0,teapotY = 0;
+GLMmodel * right_arm = NULL;
+GLMmodel * left_arm = NULL;
+int show[4] ={1,1,1,1};
+int ID = 2;
+float teapotX = 0, teapotY = 0;
+float angle = 0;
 FILE * fout = NULL;
 FILE * fin = NULL;
+void keyboard(unsigned char key,int x, int y)
+{
+    if( key=='0' ) ID=0;
+    if( key=='1' ) ID=1;
+    if( key=='2' ) ID=2;
+    if( key=='3' ) ID=3;
+    glutPostRedisplay();
+}
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-    if(head==NULL){
+    if(head==NULL)
+    {
         head = glmReadOBJ("model/head.obj");
         body = glmReadOBJ("model/body.obj");
-        uparmR = glmReadOBJ("model/uparmR.obj");
-        lowarmR = glmReadOBJ("model/lowarmR.obj");
-
+        right_arm = glmReadOBJ("model/uparmR.obj");
+        left_arm = glmReadOBJ("model/lowarmR.obj");
     }
     glPushMatrix();
-        glScalef(0.3, 0.3, 0.3);
- if (show[0]) glmDraw(head, GLM_MATERIAL);
- if (show[1])glmDraw(body, GLM_MATERIAL);
- if (show[2])glmDraw(lefthand, GLM_MATERIAL);
- if (show[3])glmDraw(righthand, GLM_MATERIAL);
+        glScalef(0.3,0.3,0.3);
+        glPushMatrix();
+            ///glTranslatef(teapotX,teapotY,0);
+            if(ID==0) glColor3f(1,0,0);
+            else glColor3f(1,1,1);
+            if(show[0]) glmDraw(head, GLM_MATERIAL);
+        glPopMatrix();
+
+        if(ID==1) glColor3f(1,0,0);
+        else glColor3f(1,1,1);
+        if(show[1]) glmDraw(body, GLM_MATERIAL);
+
+        glPushMatrix();
+            glTranslatef(-1.360000, +0.360000, 0);
+            glRotatef(angle, 0, 0, 1);
+            glTranslatef(1.360000, -0.360000, 0);
+
+            if(ID==2) glColor3f(1,0,0);
+            else glColor3f(1,1,1);
+            if(show[2]) glmDraw(right_arm, GLM_MATERIAL);
+
+            glPushMatrix();
+                ///glTranslatef(teapotX,teapotY,0);
+                glTranslatef(-1.959999, +0.080000, 0);
+                glRotatef(angle, 0, 0, 1);
+                glTranslatef(1.959999, -0.080000, 0);
+
+                if(ID==3) glColor3f(1,0,0);
+                else glColor3f(1,1,1);
+                if(show[3]) glmDraw(left_arm, GLM_MATERIAL);
+            glPopMatrix();
+        glPopMatrix();
+    glPopMatrix();
+    glColor3f(0,1,0);
+    glutSolidTeapot( 0.02 );
+    glutSwapBuffers();
 }
-void keyboard(unsigned char key, int x, int y){
-    if(key=='0') show[0]= ! show [0];
-    if(key=='1') show[1]= ! show [1];
-    if(key=='2') show[2]= ! show [2];
-    if(key=='3') show[3]= ! show [3];
-    glutPostRedisplay();
-}
+int oldX=0, oldY=0;
 void mouse(int button, int state, int x, int y)
 {
-    teapotX=(x-150)/150.0;
-    teapotY=(150-y)/150.0;
-    if(state==GLUT_DOWN){
-    if(fout==NULL) fout = fopen("file4.txt","w");
-
-    fprintf(fout, "%f %f\n", teapotX, teapotY);
+    if(state==GLUT_DOWN)
+    {
+        oldX = x;
+        oldY = y;
     }
-    display();
 }
-///void keyboard(unsigned char key, int x, int y)
-///{
-    ///if(fin==NULL){
-        ///fclose(fout);
-        ///fin = fopen("file4.txt", "r");
-    ///}
-    ///fscanf(fin, "%f%f", &teapotX, &teapotY);
-    ///display();
-///}
-int main(int argc, char *argv[])
+void motion(int x, int y)
+{
+    teapotX += (x-oldX)/150.0*3;
+    teapotY -= (y-oldY)/150.0*3;
+    printf("glTranslatef(%f, %f, 0);\n",teapotX, teapotY);
+    angle += x-oldX;
+    oldX = x;
+    oldY = y;
+    glutPostRedisplay();
+}
+int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
-    glutCreateWindow("Week12");
-
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
+    glutCreateWindow("week13");
+    glutMotionFunc(motion);
     glutDisplayFunc(display);
     glutKeyboardFunc(keyboard);
     glutMouseFunc(mouse);
